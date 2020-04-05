6619c7621c21ddbeb8cf797f0e6eff5d06e7f19e
jake oconnor <oconnor.439@osu.edu>
2020-04-05 17:00:50 -0400

    added recycler view

--- app/src/main/java/com/example/playback/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/playback/ui/home/HomeFragment.kt
@@ -9,6 +9,8 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.playback.DBManager
 import com.example.playback.R
 import com.example.playback.SpotifyPersonalData
@@ -17,6 +19,10 @@ class HomeFragment : Fragment() {
 
     private lateinit var homeViewModel: HomeViewModel
     private  val TAG = "HomeFragment"
+
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var viewAdapter: RecyclerView.Adapter<*>
+    private lateinit var viewManager: RecyclerView.LayoutManager
 
 
     override fun onCreateView(
@@ -27,13 +33,34 @@ class HomeFragment : Fragment() {
 
         Log.v(TAG,"called onCreateView")
 
-        homeViewModel =
-            ViewModelProviders.of(this).get(HomeViewModel::class.java)
+
+        homeViewModel = ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
         val textView: TextView = root.findViewById(R.id.text_home)
         homeViewModel.text.observe(this, Observer {
             textView.text = it
         })
+
+
+
+
+        var dataForRecylerView = arrayOf("TEST1","TEST2","TEST3","TEST4")
+        viewManager = LinearLayoutManager(this.activity)
+        viewAdapter = SongAdapter(dataForRecylerView)
+
+        recyclerView = root.findViewById<RecyclerView>(R.id.recycler_view).apply {
+            // use this setting to improve performance if you know that changes
+            // in content do not change the layout size of the RecyclerView
+            setHasFixedSize(true)
+
+            // use a linear layout manager
+            layoutManager = viewManager
+
+            // specify an viewAdapter (see also next example)
+            adapter = viewAdapter
+
+        }
+
         return root
     }
 
---
+++ app/src/main/java/com/example/playback/ui/home/SongAdapter.kt
@@ -0,0 +1,46 @@
+package com.example.playback.ui.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.playback.R
+import kotlinx.android.extensions.LayoutContainer
+import kotlinx.android.synthetic.main.main_page_text_view.view.*
+
+class SongAdapter (private val myDataset: Array<String>) :
+    RecyclerView.Adapter<SongAdapter.MyViewHolder>() {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder.
+    // Each data item is just a string in this case that is shown in a TextView.
+    class MyViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+        val name = itemView.songNameTextView
+        val listens = itemView.amountListenTextView
+    }
+
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(parent: ViewGroup,
+                                    viewType: Int): SongAdapter.MyViewHolder {
+        // create a new view
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.main_page_text_view, parent, false)
+        // set the view's size, margins, paddings and layout parameters
+
+        return MyViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.itemView.songNameTextView.text = myDataset[position]
+        holder.itemView.amountListenTextView.text = "0"
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = myDataset.size
+}
