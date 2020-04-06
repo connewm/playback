0ff001dfa01bbb3154013215ce73bca6d196ad84
jake oconnor <oconnor.439@osu.edu>
2020-04-06 10:49:51 -0400

    test?

--- app/src/main/java/com/example/playback/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/playback/ui/home/HomeFragment.kt
@@ -44,7 +44,7 @@ class HomeFragment : Fragment() {
 
 
 
-        var dataForRecylerView = arrayOf("TEST1","TEST2","TEST3","TEST4")
+        var dataForRecylerView = arrayOf("TEST1","TEST2","TEST3","TEST4","TEST5","TEST6","TEST7","TEST8","TEST9","TEST10","TEST11","TEST12","TEST13")
         viewManager = LinearLayoutManager(this.activity)
         viewAdapter = SongAdapter(dataForRecylerView)
 
--- app/src/main/java/com/example/playback/ui/map/MapFragment.kt
+++ app/src/main/java/com/example/playback/ui/map/MapFragment.kt
@@ -11,6 +11,23 @@ import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.example.playback.R
 
+
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Cartesian;
+import com.anychart.charts.Pie
+import com.anychart.core.cartesian.series.Column;
+import com.anychart.enums.Anchor;
+import com.anychart.enums.HoverMode;
+import com.anychart.enums.Position;
+import com.anychart.enums.TooltipPositionMode;
+//import com.anychart.sample.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class MapFragment : Fragment() {
 
     private lateinit var mapViewModel: MapViewModel
@@ -23,13 +40,31 @@ class MapFragment : Fragment() {
     ): View? {
 
         Log.v(TAG,"called onCreateView")
-        mapViewModel =
-            ViewModelProviders.of(this).get(MapViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_maps, container, false)
+
+        /*mapViewModel =
+            ViewModelProviders.of(this).get(MapViewModel::class.java)
         val textView: TextView = root.findViewById(R.id.map_page)
         mapViewModel.text.observe(this, Observer {
             textView.text = it
         })
+        */
+
+        var pie : Pie = AnyChart.pie()
+        var data = ArrayList<DataEntry>()
+
+        data.add(  ValueDataEntry("John", 10000))
+        data.add(ValueDataEntry("Jake", 12000))
+        data.add( ValueDataEntry("Peter", 18000))
+
+        pie.data(data);
+
+       var anyChartView:  AnyChartView  = root.findViewById(R.id.any_chart_view);
+        anyChartView.setChart(pie)
+
+
+
+
         return root
     }
 }
