357dcd0da50f6ea48b370061ad16172a3903989c
connewm <newman.556@osu.edu>
2020-04-06 15:28:31 -0400

    asdf

--- app/src/main/java/com/example/playback/ui/database_test/DBContract.kt
+++ app/src/main/java/com/example/playback/ui/database_test/DBContract.kt
@@ -6,10 +6,13 @@ object DBContract{
     class DataEntry:BaseColumns{
         companion object {
             val TABLE_NAME = "personalSpotifyDB"
-            val COLUMN_RECORD_ID = "recordid"
-            val COLUMN_USER_ID = "userid"
-            val COLUMN_ARTIST_NAME = "artistname"
-            val COLUMN_POPULARITY_SCORE = "popularityscore"
+            val COLUMN_RECORD_ID = "record_id"
+            val COLUMN_USER_ID = "user_id"
+            val COLUMN_ARTIST_NAME = "artist_name"
+            val COLUMN_POPULARITY_SCORE = "popularity_score"
+            val COLUMN_SONG_NAME = "song_name"
+            val COLUMN_ALBUM_NAME = "album_name"
+            val COLUMN_SONG_GENRE = "song_genre"
         }
     }
 }
